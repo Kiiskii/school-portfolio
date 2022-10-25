101a3685ea995c6152bc33d1c3ab41458efc7f6a
Anton Kiiski <antonoliverkiiski@gmail.com>
2022-10-25 19:23:01 +0300

    Installed dotnet on PC, started new format for index.html

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 let iframe = document.getElementById("project");
 console.log(iframe);
-let links = document.querySelectorAll("article li a");
-console.log(links);
+let links = document.querySelectorAll("nav li a");
+console.log(links)
+
+function myFunction() {
+  document.getElementById("dropdown1").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdownContent");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 
 links.forEach((link) => {
   link.addEventListener("click", (event) => {
